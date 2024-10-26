a5882c1adce5e0232be71af059130a4a158c8271
Jason LaPierre <jalapierre@wm.edu>
2024-10-26 15:18:26 -0400

    Adding background SVG and the start to fractal noise

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -72,6 +72,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'noise': 'url("../images/noise.svg")'
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
