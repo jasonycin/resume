1b90d4c7a74f703254e050a4a80e3a8fe26f0bfa
Jason LaPierre <jalapierre@wm.edu>
2024-10-27 14:19:08 -0400

    Add LinkedIn and GitHub icons, overlay noise over all elements including text and pictures

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,6 +19,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        gray: "#393939",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
